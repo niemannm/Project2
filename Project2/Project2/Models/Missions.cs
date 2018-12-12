82d9be4d9c1c648a904d5f7213db67e5f62e747c
Michael Niemann <niemannmichaeld@gmail.com>
2018-12-11 17:05:36 -0700

    models

--- Project2/Project2/Models/Missions.cs
+++ Project2/Project2/Models/Missions.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Project2.Models
 {
+    [Table("Missions")]
     public class Missions
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ClientID { get; set; }
+        [Required(ErrorMessage = "Please enter your first name")]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name")]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter an address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter your state")]
+        public string State { get; set; }
+
+        [DisplayName("Zipcode")]
+        [RegularExpression(@"^\d{5}([\-]\d{4})?$", ErrorMessage = "Please enter a 5 digit zip code")]
+        [Required(ErrorMessage = "Please enter your zip code")]
+        public int Zip { get; set; }
+        [Required(ErrorMessage = "Please enter your email")]
+
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a phone number")]
+        [Phone]
+        [RegularExpression(@"^\+?\(?\d+\)?(\s|\-|\.)?\d{1,3}(\s|\-|\.)?\d{4}[\s]*[\d]*$", ErrorMessage = "Please enter a valid phone number (123) 456-7890")]
+        public string Phone { get; set; }
     }
 }
