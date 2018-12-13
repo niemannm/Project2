c96185e15a8e2e3111a3912d6a3b50385db65e80
SAMGINES135A\samgines <sam.gines.104@gmail.com>
2018-12-12 17:20:07 -0700

    do work son

--- Project2/Project2/Controllers/HomeController.cs
+++ Project2/Project2/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Project2.DAL;
+using Project2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Project2.Controllers
 {
     public class HomeController : Controller
     {
+        private MissionContext db = new MissionContext();
+        public static int userRole = 0;
+
         public ActionResult Index()
         {
             return View();
@@ -29,6 +35,56 @@ namespace Project2.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View("SignUp");
+        }
+
+        [HttpPost]
+        public ActionResult SignUp([Bind(Include = "UserID,UserEmail,Password,FirstName,LastName")] Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User.Add(users);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(users);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Username, string Password, bool rememberMe = false)
+        {
+            var currentUser = db.Database.SqlQuery<Users>(
+            "Select * " +
+            "FROM Users " +
+            "WHERE UserEmail = '" + Username + "' AND " +
+            "Password = '" + Password + "'");
+            if (currentUser.Count() > 0)
+            {
+                if (Username == "sam.gines.104@gmail.com")
+                {
+                    userRole = 9001;
+                }
+                else
+                {
+                    userRole = 1;
+                }
+                FormsAuthentication.SetAuthCookie(Username, rememberMe);
+                return RedirectToAction("Index", "Home", new { userlogin = Username });
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
